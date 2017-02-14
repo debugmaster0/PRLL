60e6c79a561c4381dfad1fcf8ab291faf54bc9f1
debugmaster0 <brucejmartin.bm@gmail.com>
2017-02-13 21:27:59 -0800

    Update PA01.cpp

--- PA01.cpp
+++ PA01.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &mrank);
 	nxtRank = (mrank + 1) % 2;
 	
-	MPI_Comm_Size(MPI_COMM_WORLD, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
 	pingCount = 0;
 
